4c012c2142229e79e033b0d9f4685592c9435816
14bc034d <53335706+14bc034d@users.noreply.github.com>
2021-02-05 13:19:28 +0100

    Добавлена анимация
    Теперь игрок ходит с анимацией.

--- assets/script/move.gd
+++ assets/script/move.gd
@@ -4,23 +4,25 @@ export (int) var speed = 300
 
 var velocity = Vector2()
 
+func animates_player(direction: Vector2):
+	if direction != Vector2.ZERO:
+		$Sprite.play("walk")
+	else:
+		$Sprite.play("default")
+
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed('right'):
 		velocity.x += 1
-		#$Sprite.animation("walk")
 	if Input.is_action_pressed('left'):
 		velocity.x -= 1
-		#$Sprite.animation("walk")
 	if Input.is_action_pressed('down'):
 		velocity.y += 1
-		#$Sprite.animation("walk")
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
-		#$Sprite.animation("walk")
 	velocity = velocity.normalized() * speed
-	#$Sprite.animation("default")
 
 func _physics_process(delta):
 	get_input()
+	animates_player(velocity)
 	velocity = move_and_slide(velocity)
