b28b38460b5b57bd19b642fddbe0b0cf3dea989d
14bc034d <53335706+14bc034d@users.noreply.github.com>
2021-02-26 01:13:31 +0100

    Штуки с атакой(молниями)

--- assets/script/lightning_bolt.gd
+++ assets/script/lightning_bolt.gd
@@ -1,4 +1,20 @@
 extends KinematicBody2D
 
+func wait(sec):
+	var t = Timer.new()
+	t.set_wait_time(sec)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	
+
 func _physics_process(delta):
-	rotation += get_local_mouse_position().angle()*0.1
+	rotation += get_local_mouse_position().angle()
+	var t = Timer.new()
+	t.set_wait_time(1)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	self.get_parent().remove_child(self)
--- assets/script/shoot.gd
+++ assets/script/shoot.gd
@@ -1,11 +1,15 @@
 extends Node
 
-var level
+var has_bolt
 
 func shoot():
-	var projectile = load("res://assets/scene/lightning_bolt.tscn")
-	var lightning_bolt = projectile.instance()
-	self.add_child(lightning_bolt)
+	has_bolt = false
+	for _i in self.get_children():
+		if("lightning_bolt" in _i.name): has_bolt = true
+	if(!has_bolt):
+		var projectile = load("res://assets/scene/lightning_bolt.tscn")
+		var lightning_bolt = projectile.instance()
+		self.add_child(lightning_bolt)
 
 func _ready():
 	pass
@@ -13,3 +17,9 @@ func _ready():
 func _process(delta):
 	if(Input.is_action_pressed('mouse_left')):
 		shoot()
+		var t = Timer.new()
+		t.set_wait_time(5)
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
